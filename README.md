233a95d1a0182988a87fa004380388f8446f3130
Radoslaw Janowski <janowski@proximus-software.com>
2013-05-13 15:49:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+Zenoss-SMS-EAGLE
+================
+
+Plugin for Zenoss to send SMS Alerts with SMSEagle device (http://www.smseagle.eu)
+
+Script source: https://bitbucket.org/proximus/smseagle-zenoss/
+
+Published on BSD License
+
+
+Installation instructions
+-------------------------
+
+#### SMSEAGLE SETUP
+
+Create a new user for this script in SMSEagle.
+
+
+#### ZENOSS SETUP
+
+1. Download latest version of Zenoss script from: https://bitbucket.org/proximus/smseagle-zenoss
+
+
+2. Edit following lines in the script:
+
+    SMSEAGLE_USER = "john"
+    SMSEAGLE_PASSWORD = "doe"
+    SMSEAGLE_IP = "192.168.0.101"
+
+
+Save the script to the location: $ZENHOME/bin/zenoss_smseagle.py (where $ZENHOME is your Zenoss directory). Ensure that it's executable (chmod 755 zenoss_smseagle.py).
+
+
+3. Add a cell phone number to the "Pager" field of each Zenoss user account.
+
+
+4. Go to Advanced->Settings and modify the "Page Command" to: 
+ 
+     $ZENHOME/bin/zenoss_smseagle.py $RECIPIENT
+
+
+5. When finished, test by using the "test" link next to the cell number
+shown in the Pager column of each user.
+
+
+6. If testing is successful (a SMS message is received at the cell phone) then create alerts and specify "page" as the action.
+	
+
---
+++ zenoss_smseagle.py
@@ -0,0 +1,94 @@
+#! /usr/bin/env python
+# ============================== SUMMARY =====================================
+#
+# Program : zenoss_smseagle.py
+# Version : 1.0
+# Date : May 13 2013
+# Author : SMSEagle Team
+# Summary : This plugin sends ZenOss SMS alerts with SMSEagle hardware sms gateway
+# License : BSD
+#
+# ============================= MORE INFO ======================================
+#
+# Visit: http://www.smseagle.eu
+# The latest version of this plugin can be found on:
+# http://bitbucket.org/proximus/smseagle-zenoss
+#
+# ============================== SETUP =========================================
+#
+# 1. Create a user/password in SMSEagle
+#
+# 2. Edit variables SMSEAGLE_USER, SMSEAGLE_PASSWORD, SMSEAGLE_IP in code below
+#
+# 3. Configure Zenoss (see tutorial http://www.smseagle.eu/plugins.php)
+#
+# ==============================================================================
+# If logs are enabled they are saved to $ZENHOME/log/sms_smseagle.log
+# where $ZENHOME is your Zenoss directory
+
+
+import os
+import sys
+import time
+import urllib
+import urllib2
+
+# Edit these settings to match your own
+SMSEAGLE_USER = "john"
+SMSEAGLE_PASSWORD = "doe"
+SMSEAGLE_IP = "192.168.0.101"
+LOG_ENABLED = 0
+#
+
+def main():
+
+    try:
+       	# Read recipient number
+        rcpt = sys.argv[1]
+    except:
+        print "Invalid arguments! Usage: zenoss_smseagle <recipient>"
+        sys.exit(1)
+    
+    try:
+	# Open logfile if logging enabled
+       	if LOG_ENABLED:
+            file = os.environ['ZENHOME']+"/log/sms_smseagle.log"
+            log = open(file, 'a')
+    except:
+        print "Cannot open log file!"
+        sys.exit(2) 
+    	
+    try:
+       	# Read message from standard in
+       	msg = sys.stdin.read()
+
+       	# Prepare HTTP request
+       	base_url = "http://"+SMSEAGLE_IP+"/index.php/http_api/send_sms"
+       	query_args = { 'login':SMSEAGLE_USER, 'pass':SMSEAGLE_PASSWORD, 'to':rcpt, 'message':msg}
+       	encoded_args = urllib.urlencode(query_args)
+       	url = base_url + '?' + encoded_args
+       	
+       	# Write log if logging enabled
+       	if LOG_ENABLED:
+       	    timestamp = "["+time.strftime("%Y-%m-%d %H:%M:%S")+"]"
+       	    log.write("%s ===== BEGIN SENDING SMS ==== \n" % timestamp)			
+       	    log.write("%s SMS recipient: %s\n" % (timestamp, rcpt))
+       	    log.write("%s SMS text: %s\n" % (timestamp, msg))
+			
+       	#HTTP request to SMSEagle
+       	result = urllib2.urlopen(url).read()
+    	
+        # Write log if logging enabled
+        if LOG_ENABLED:
+            timestamp = "["+time.strftime("%Y-%m-%d %H:%M:%S")+"]"
+            try:
+                log.write("%s Sending result: %s\n" % (timestamp, result))
+                log.write("%s ===== END SENDING SMS ====\n" % timestamp)
+            finally:
+                log.close()
+    except Exception, e:
+        print e
+        sys.exit(1)
+		
+    
+main()
